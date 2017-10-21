0e95ed5d1991a3716c9454ee6b71cf636d68ee46
Ivan Ivanov <ipivanov11@gmail.com>
2017-10-21 20:03:59 +0300

    removed scrape

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,17 +1,17 @@
-var puppeteer = require('puppeteer');
+// var puppeteer = require('puppeteer');
 
-module.exports = async function scrape(url, selector) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+// module.exports = async function scrape(url, selector) {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
 
-  await page.setViewport({ width: 1025, height: 768 });
-  await page.goto(url);
-  //await page.screenshot({ path: 'screenshots/github.png' });
-  const scrapedText = await page.evaluate((selector) => {
-    return document.body.querySelector(selector).textContent
-  }, selector);
+//   await page.setViewport({ width: 1025, height: 768 });
+//   await page.goto(url);
+//   //await page.screenshot({ path: 'screenshots/github.png' });
+//   const scrapedText = await page.evaluate((selector) => {
+//     return document.body.querySelector(selector).textContent
+//   }, selector);
 
-  browser.close();
+//   browser.close();
 
-  return scrapedText;
-}
+//   return scrapedText;
+// }
