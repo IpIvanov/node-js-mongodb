44b1ae448de96c69bc93ccfac8d448b82faf2214
Ivan Ivanov <ipivanov11@gmail.com>
2017-10-21 19:11:09 +0300

    Added cheerio to the project

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const scrape = require('./scripts/scrape');
 const express = require('express');
 const app = express();
 const cron = require('node-cron');
+const request = require('request');
+const cheerio = require('cheerio');
 
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://herokuUser:user332211@ds117965.mlab.com:17965/heroku_jj0khzm2";
+const mongoURL = "mongodb://herokuUser:user332211@ds117965.mlab.com:17965/heroku_jj0khzm2";
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,32 +17,56 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
 
 cron.schedule('* * * * *', function() {
-  console.log('running a task every minute');
-  scrape('https://github.com', 'h1.lh-condensed-ultra').then(res => {
-    MongoClient.connect(url, function(err, db) {
-      if (err) throw err;
-      console.log("Database created!");
-      console.log(res, '--> Scraped text from https://github.com')
+    console.log('running a task every minute');
+    const signs = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
+    // scrape('https://github.com', 'h1.lh-condensed-ultra').then(res => {
+    //   MongoClient.connect(url, function(err, db) {
+    //     if (err) throw err;
+    //     console.log("Database created!");
+    //     console.log(res, '--> Scraped text from https://github.com')
 
-      // const myobj = { name: "Company Inc", address: "Highway 37" };
-      // db.collection("customers").insertOne(myobj, function(err, res) {
-      //     if (err) throw err;
-      //     console.log("1 document inserted");
-      //     db.close();
-      // });
-    });
-  });
-  scrape('https://www.npmjs.com/', '.title.em-default.type-neutral-11').then(res => {
-    console.log(res.trim(), '--> Scraped text from https://www.npmjs.com/')
-  });
+    //     const myobj = { name: "Company Inc", address: "Highway 37" };
+    //     db.collection("customers").insertOne(myobj, function(err, res) {
+    //         if (err) throw err;
+    //         console.log("1 document inserted");
+    //         db.close();
+    //     });
+    //   });
+    // });
+
+
+    let dailyHoroscopes = [];
+    for (let i = 0; i < signs.length; i++) {
+        let url = 'https://www.ganeshaspeaks.com/horoscopes/daily-horoscope/' + signs[i];
+        request(url, function(err, resp, body) {
+            if (err) throw err;
+            $ = cheerio.load(body);
+            let info = $('#daily > div > div.col.m12.l9.padding-right-35.padding-right-sm-0 > div.row.margin-bottom-0 > p.margin-top-xs-0').text().trim()
+            let key = signs[i];
+            let obj = {};
+            obj[key] = info;
+
+            console.log(obj)
+                // TODO: scraping goes here!
+            MongoClient.connect(mongoURL, function(err, db) {
+                if (err) throw err;
+                console.log("Database created!");
+                db.collection("signs-daily").insertOne(obj, function(err, res) {
+                    if (err) throw err;
+                    console.log("1 document inserted");
+                    db.close();
+                });
+            });
+        });
+    }
 });
 
 //exceute every 1 min
