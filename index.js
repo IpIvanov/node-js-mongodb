98fac4006cbe41f6b07c716df2d80e37f85c123e
Ivan Ivanov <ipivanov11@gmail.com>
2017-10-22 22:29:01 +0300

    changed cron time

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
 
-cron.schedule('* * * * *', function() {
+cron.schedule('0 4 * * *', function() {
     MongoClient.connect(mongoURL, function(err, db) {
         if (err) throw err;
         db.collection('signs').remove();
